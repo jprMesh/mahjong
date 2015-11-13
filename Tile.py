19aab17dd7801679d3d3cc70e2d21b623fe9864e
hibuddies <jdeng4@gmail.com>
2015-11-13 01:54:21 -0500

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -1,4 +1,7 @@
 class Tile:
-	name = ""			#tile ID
-	suit = ""			#dot, bamboo, character, honor
-	value = ""		#1-9, R, G, B, E, S, W, N
+	suit = ""	#dot, bamboo, character, honor
+	value = ""	#1-9, R, G, B, E, S, W, N, flower, season
+	def __init__(self, suit, value):
+		self.suit = suit
+		self.value = value
+	
