793ea7820c8368697c9595ed1cd19e7f9d1f9709
jprmesh <mrachhh@gmail.com>
2015-11-13 09:40:04 -0500

    Fixed indentation and minor cleanup.

--- Tile.py
+++ Tile.py
@@ -1,7 +1,6 @@
 class Tile:
-	suit = ""	#dot, bamboo, character, honor
-	value = ""	#1-9, R, G, B, E, S, W, N, flower, season
-	def __init__(self, suit, value):	#i have no idea if this is how you make a constructor
-		self.suit = suit
-		self.value = value
-	
+    suit = ""   #dot, bamboo, character, honor
+    value = ""  #1-9, R, G, B, E, S, W, N, flower, season
+    def __init__(self, suit, value):    #i have no idea if this is how you make a constructor
+        self.suit = suit
+        self.value = value
--- mahjong.py
+++ mahjong.py
@@ -1,12 +1,12 @@
-from mahjong import Tile
+from Tile import Tile
 
 suits = ["Bamboo", "Dot", "Character", "Honor"]
 honorValues = ["E", "S", "W", "N", "R", "G", "B", "Flower", "Season"]
 wall = []   #initialize empty wall list
 
-for i in range(0,4):    #4 copies of every tile
-    for suit in suits[0:3]:     #for suited tiles
-        for n in range(1,10):   #numbers 1-9
+for i in range(4):    #4 copies of every tile
+    for suit in suits[:3]:     #for suited tiles
+        for n in range(10):   #numbers 1-9
             tile = Tile(suit=suit, value=n)
             wall.append(tile)
     for value in honorValues:   #for honor tiles
