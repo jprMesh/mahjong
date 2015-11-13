abf228fa932e00092e990a253311e41a57e63b47
hibuddies <jdeng4@gmail.com>
2015-11-13 02:24:59 -0500

    Update mahjong.py

--- mahjong.py
+++ mahjong.py
@@ -4,7 +4,7 @@ suits = ["Bamboo", "Dot", "Character", "Honor"]
 honorValues = ["E", "S", "W", "N", "R", "G", "B", "Flower", "Season"]
 wall = []   #initialize empty wall list
 
-for i in range(0,3):    #4 copies of every tile
+for i in range(0,4):    #4 copies of every tile
     for suit in suits[0:3]:     #for suited tiles
         for n in range(1,10):   #numbers 1-9
             tile = Tile(suit=suit, value=n)
