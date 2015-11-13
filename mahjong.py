cfa7c3c22d0e351b16c6f7e10756519ba1446252
hibuddies <jdeng4@gmail.com>
2015-11-13 01:49:34 -0500

    Update mahjong.py

--- mahjong.py
+++ mahjong.py
@@ -1,2 +1,14 @@
-if __name__ == "__main__":
-    print("bruh")
+from mahjong import Tile
+
+suits = ["Bamboo", "Dot", "Character", "Honor"]
+honorValues = ["E", "S", "W", "N", "R", "G", "B", "Flower", "Season"]
+wall = []   #initialize empty wall list
+
+for i in range(0,3):    #4 copies of every tile
+    for suit in suits[0:3]:     #for suited tiles
+        for n in range(1,10):   #numbers 1-9
+            tile = Tile(suit=suit, value=n)
+            wall.append(tile)
+    for value in honorValues:   #for honor tiles
+        tile = Tile(suit="Honor", value=value)
+        wall.append(tile)
