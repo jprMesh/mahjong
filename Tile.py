85b11510c38d06938a4b0970ae749f117f593bfa
hibuddies <jdeng4@gmail.com>
2015-11-16 20:45:55 -0500

    added equals() method

--- Tile.py
+++ Tile.py
@@ -1,6 +1,8 @@
 class Tile:
     suit = ""   #dot, bamboo, character, honor
     value = ""  #1-9, R, G, B, E, S, W, N, flower, season
-    def __init__(self, suit, value):    #i have no idea if this is how you make a constructor
+    def __init__(self, suit, value):   
         self.suit = suit
         self.value = value
+    def __eq__(self, other):
+    	return self.__dict__ == other.__dict__
