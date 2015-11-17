e5df41aee796bd91ea214acce3fcd766f4b7d443
jprmesh <mrachhh@gmail.com>
2015-11-16 21:32:30 -0500

    Made game setup more efficient.

--- Mahjong.py
+++ Mahjong.py
@@ -2,6 +2,7 @@ from Tile import Tile
 from Players import Players
 from random import randint
 from random import shuffle
+from random import choice
 
 #Dice rolling function
 #3 dice are rolled instead of one 18 sided die
@@ -13,35 +14,25 @@ def diceRoll():
     totalSum = dice1 + dice2 + dice3
     return totalSum
 
-suits = ["Bamboo", "Dot", "Character"]
-honorValues = ["E", "S", "W", "N", "R", "G", "B", "Flower", "Season"]
+suits = ["Bamboo", "Dot", "Character", "Honor"]
 winds = ["East", "South", "West", "North"]
-wall = []   #initialize empty wall list
-hand = []
-diceRolls = []
-prevailingWind = "" 
-players = []
+honorValues = ["E", "S", "W", "N", "R", "G", "B", "Flower", "Season"]
 
-#Populate the tiles list
-for i in range(4):    #4 copies of every tile
-    for suit in suits:     #for suited tiles
-        for n in range(1,10):   #numbers 1-9
-            tile = Tile(suit=suit, value=n)
-            wall.append(tile)
-    for value in honorValues:   #for honor tiles
-        tile = Tile(suit="Honor", value=value)
-        wall.append(tile)
-shuffle(wall)
+class Mahjong():
+    """Game management class"""
+    def __init__(self):
+        self.generateWall()
+        #self.addPlayers()
+        self.prevailingWind = choice(winds)
 
-#set up the Players
-eastPlayer = Players(wind="")
-players.append(eastPlayer)
+    def generateWall(self):
+        self.wall = [Tile(suit, val+1+(suit=="Honor")*10)
+                for suit in suits
+                for val in xrange(9)
+                for x in xrange(4)]
+        shuffle(self.wall)
 
-southPlayer = Players(wind="")
-players.append(southPlayer)
+    def addPlayers(self):
+        self.players = [Players(wind) for wind in winds]
 
-westPlayer = Players(wind="")
-players.append(westPlayer)
-
-northPlayer = Players(wind="")
-players.append(northPlayer)
+Mahjong()
