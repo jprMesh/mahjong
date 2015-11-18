04d18dc4932bdcdfd553d3a66219589249398508
jprmesh <mrachhh@gmail.com>
2015-11-17 22:09:37 -0500

    Moved rest of globals to Mahjong class.

--- Mahjong.py
+++ Mahjong.py
@@ -1,38 +1,37 @@
 from Tile import Tile
 from Players import Players
-from random import randint
-from random import shuffle
-from random import choice
+import random
 
-#Dice rolling function
-#3 dice are rolled instead of one 18 sided die
-#This probably makes dice graphics easier later
-def diceRoll():
-    dice1 = Random.randint(1,6)
-    dice2 = Random.randint(1,6)
-    dice3 = Random.randint(1,6)
-    totalSum = dice1 + dice2 + dice3
-    return totalSum
-
-suits = ["Bamboo", "Dot", "Character", "Honor"]
-winds = ["East", "South", "West", "North"]
-honorValues = ["E", "S", "W", "N", "R", "G", "B", "Flower", "Season"]
-
-class Mahjong():
+class Mahjong:
     """Game management class"""
+
+    # Static Variables
+    suits = ["Bamboo", "Dot", "Character", "Honor"]
+    winds = ["East", "South", "West", "North"]
+    honor_values = ["E", "S", "W", "N", "R", "G", "B", "Flower", "Season"]
+
+    @staticmethod
+    def roll3d6():
+        dice1 = Random.randint(1,6)
+        dice2 = Random.randint(1,6)
+        dice3 = Random.randint(1,6)
+        totalSum = dice1 + dice2 + dice3
+        return totalSum
+
     def __init__(self):
         self.generateWall()
         self.addPlayers()
-        self.prevailingWind = choice(winds)
+        self.prevailingWind = "East"
 
     def generateWall(self):
         self.wall = [Tile(suit, val+1+(suit=="Honor")*10)
-                for suit in suits
+                for suit in Mahjong.suits
                 for val in xrange(9)
                 for x in xrange(4)]
-        shuffle(self.wall)
+        random.shuffle(self.wall)
 
     def addPlayers(self):
-        self.players = [Players(wind, self.wall) for wind in winds]
+        self.players = [Players(wind, self.wall) for wind in Mahjong.winds]
 
-Mahjong()
+game = Mahjong()
+print game.players[0].hand
