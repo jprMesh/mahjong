ed908a8596c75446062978281210d62487a5976e
jprmesh <mrachhh@gmail.com>
2015-12-19 16:54:11 -0500

    Added test case for the handContains method.

--- MahjongTest.py
+++ MahjongTest.py
@@ -1,4 +1,6 @@
 import unittest
+import random
+from Tile import Tile
 from Mahjong import Mahjong
 
 class MahjongTest(unittest.TestCase):
@@ -14,10 +16,20 @@ class MahjongTest(unittest.TestCase):
         self.assertEqual(self.mjgame.prevailingWind, "East")
 
     def testWall(self):
-        self.assertEqual(len(self.mjgame.wall), 4*4*9-(13*4))
+        self.assertEqual(len(self.mjgame.wall), (4*4*9)-(13*4))
 
     def testHand(self):
         self.assertEqual(len(self.mjgame.players[0].hand), 13)
+
+    def testHandContains(self):
+        for x in range(13):
+            self.mjgame.players[0].hand.pop()
+        randvalue = random.randint(1,9)
+        randtile = Tile(random.choice(["Bamboo", "Dot", "Character", "Honor"]), randvalue)
+        self.mjgame.players[0].hand.append(randtile)
+        self.assertTrue(self.mjgame.players[0].handContains(randvalue))
+        self.mjgame.players[0].hand.pop()
+        self.assertFalse(self.mjgame.players[0].handContains(randvalue))
 
 if __name__ == '__main__':
     unittest.main()
