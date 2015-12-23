c85d2bdd37be7451c49e398f0e3c5c130c3743a4
jprmesh <mrachhh@gmail.com>
2015-12-23 15:54:02 -0500

    Big changes to TextMahjong. Can kinda call pungs and chis now.

--- TextMahjong.py
+++ TextMahjong.py
@@ -7,40 +7,81 @@ def isInt(s):
     except ValueError:
         return False
 
-if __name__ == '__main__':
-    gg = Mahjong()
-    print("Welcome to HK Mahjong.\nThe prevailing wind is currently "+
-          Mahjong.winds[gg.prevailingWind]+
-          ". The move is to the "+Mahjong.winds[gg.turnIndic]+" player.")
+def printHand(gg):
+    print(Mahjong.winds[gg.turnIndic]+", your hand is now:")
+    for tilenum in range(len(gg.players[gg.turnIndic].hand)):
+        print(chr(tilenum+97)+"  "+str(gg.players[gg.turnIndic].hand[tilenum]))
+
+def drawphase(gg):
+    printHand(gg)
+    if gg.pile:
+        print("The top of the discard pile is: "+str(gg.pile[-1]))
+    else:
+        print("There is no discard pile.")
+    drawfrom = input("From where would you like to draw?\n> ")
+    if drawfrom.lower() == "wall":
+        gg.players[gg.turnIndic].draw(gg.wall)
+    elif drawfrom.lower() == "pile" and gg.pile:
+        gg.players[gg.turnIndic].draw(gg.pile)
+    else:
+        print("Invalid draw location")
+        return
+    gg.phase = "discard"
+
+def discardphase(gg):
+    printHand(gg)
+    discardIndex = input("Which tile would you like to discard?\n> ")
+    if discardIndex[0].lower() and ord(discardIndex[0])-97 < len(gg.players[gg.turnIndic].hand):
+        gg.players[gg.turnIndic].discard(gg.players[gg.turnIndic].hand[ord(discardIndex[0])-97], gg)
+        print(Mahjong.winds[(gg.turnIndic-1)%4]+" discarded "+str(gg.pile[-1])+".")
+        gg.phase = "pungchi"
+
+def pungchiphase(gg):
+    pungs = input("Players calling pung?\n> ")
+    if pungs:
+        for x in range(3):
+            nextplayer = (gg.turnIndic+1+x)%4
+            if str(nextplayer) in pungs:
+                gg.players[nextplayer].draw(gg.pile)
+                gg.turnIndic = nextplayer
+                gg.phase = "flipup"
+                return
+    chis = input("Players calling chi?\n> ")
+    if chis:
+        for x in range(3):
+            nextplayer = (gg.turnIndic+1+x)%4
+            if str(nextplayer) in chis:
+                gg.players[nextplayer].draw(gg.pile)
+                gg.turnIndic = nextplayer
+                gg.phase = "flipup"
+                return
+    gg.phase = "draw"
+
+def flipupphase(gg):
+    printHand(gg)
+    gg.players[gg.turnIndic].putFaceUp(gg.players[gg.turnIndic].hand[-1])
     while 1:
+        fliptiles = input("Which other tiles would you like to flip up?\n> ")
+        if len(fliptiles) is 2:
+            for x in fliptiles:
+                if x.lower() and ord(x)-97 < len(gg.players[gg.turnIndic].hand):
+                    gg.players[gg.turnIndic].putFaceUp(gg.players[gg.turnIndic].hand[ord(x)-97])
+            break
+    gg.phase = "discard"
 
-        # Draw phase
-        print("\n"+Mahjong.winds[gg.turnIndic]+", this is your hand:")
-        for item in gg.players[gg.turnIndic].hand:
-            print(item)
-        if gg.pile:
-            print("The top of the discard pile is: "+str(gg.pile[-1]))
-        else:
-            print("There is no discard pile.")
-        while 1:
-            drawfrom = input("From where would you like to draw?\n> ")
-            if drawfrom.lower() == "wall":
-                gg.players[gg.turnIndic].draw(gg.wall)
-                break
-            elif drawfrom.lower() == "pile" and gg.pile:
-                gg.players[gg.turnIndic].draw(gg.pile)
-                break
-            else:
-                print("Invalid draw location")
-
+if __name__ == '__main__':
+    mgame = Mahjong()
+    print("Welcome to HK Mahjong.\nThe prevailing wind is currently "+
+          Mahjong.winds[mgame.prevailingWind]+
+          ". The move is to the "+Mahjong.winds[mgame.turnIndic]+" player.")
+    
+    while 1:
+        if mgame.phase is "draw":
+            drawphase(mgame)
         # Do some sort of win condition checking here?
-
-        # Discard phase
-        print("Your hand is now:")
-        for tilenum in range(len(gg.players[gg.turnIndic].hand)):
-            print(str(tilenum).rjust(2)+"  "+str(gg.players[gg.turnIndic].hand[tilenum]))
-        while 1:
-            discardID = input("Which tile would you like to discard?\n> ")
-            if isInt(discardID) and int(discardID) < len(gg.players[gg.turnIndic].hand):
-                gg.players[gg.turnIndic].discard(gg.players[gg.turnIndic].hand[int(discardID)], gg)
-                break
+        elif mgame.phase is "discard":
+            discardphase(mgame)
+        elif mgame.phase is "pungchi":
+            pungchiphase(mgame)
+        elif mgame.phase is "flipup":
+            flipupphase(mgame)
